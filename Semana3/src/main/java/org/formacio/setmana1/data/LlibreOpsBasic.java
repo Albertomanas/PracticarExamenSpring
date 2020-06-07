b3d66e6823655b82ea98ba5a5763ce7ac0bf7d8a
Albertomanas <amanas@cifpfbmoll.eu>
2020-06-07 18:44:42 +0200

    feat(todo): Hecho el programa con éxito, pasa los casos test

--- Semana3/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
+++ Semana3/src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
@@ -1,26 +1,48 @@
 package org.formacio.setmana1.data;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
+import org.springframework.stereotype.Repository;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+@Repository
 public class LlibreOpsBasic {
 	
+	@PersistenceContext
+	private EntityManager em;
 	/**
 	 * Retorna el llibre amb l'ISBN indicat o, si no existeix, llança un LlibreNoExisteixException
 	 */
-	public Llibre carrega (String isbn) throws LlibreNoExisteixException {
-		return null;
+	public Llibre carrega (String isbn) throws LlibreNoExisteixException { 
+
+		Llibre llibre = em.find(Llibre.class, isbn);
+		if (llibre != null) {
+			return llibre;
+		} else {
+			throw new LlibreNoExisteixException();
+		}
 	}
 	
 	/**
 	 * Sense sorpreses: dona d'alta un nou llibre amb les propietats especificaques
 	 */
+	@Transactional
 	public void alta (String isbn, String autor, Integer pagines, Recomanacio recomanacio, String titol) {
+		Llibre llibre = new Llibre();
+		llibre.setIsbn(isbn);
+		llibre.setAutor(autor);
+		llibre.setTitol(titol);
+		llibre.setPagines(pagines);
+		llibre.setRecomanacio(recomanacio);
+		em.persist(llibre);
 	}
 	
 	/**
@@ -28,29 +50,57 @@ public class LlibreOpsBasic {
 	 * @param isbn del llibre a eliminar
 	 * @return true si s'ha esborrat el llibre, false si no existia
 	 */
+	@Transactional
 	public boolean elimina (String isbn) {
-		return true;
-	}
+		
+		try {
+			Llibre llibre = this.carrega(isbn);
+			em.remove(llibre);
+			return true;
+		} catch (LlibreNoExisteixException e) {
+			return false;
+		}
+		}
 	
 	/**
 	 * Guarda a bbdd l'estat del llibre indicat
 	 */
+	@Transactional
 	public void modifica (Llibre llibre) {
+		em.merge(llibre);
 	}
 	
 	/**
 	 * Retorna true o false en funcio de si existeix un llibre amb aquest ISBN
 	 * (Aquest metode no llanca excepcions!)
+	 * @throws LlibreNoExisteixException 
 	 */
+	@Transactional
 	public boolean existeix (String isbn) {
-		return false;
+		
+		try {
+			Llibre llibre = this.carrega(isbn);
+			return true;
+		} catch (LlibreNoExisteixException e) {
+			return false;
+		}
 	}
 
 	/**
 	 * Retorna quina es la recomanacio per el llibre indicat
 	 * Si el llibre indicat no existeix, retorna null
 	 */
-	public Recomanacio recomenacioPer (String isbn) {
+	@Transactional
+	public Recomanacio recomenacioPer (String isbn){
+
+		try {
+			if (this.existeix(isbn)) {
+				Llibre llibre = this.carrega(isbn);
+				return llibre.getRecomanacio();
+			}
+		} catch (LlibreNoExisteixException e) {
+			return null;
+		}
 		return null;
 	}
 	
--- Semana3/src/main/java/org/formacio/setmana1/mvc/LlibreController.java
+++ Semana3/src/main/java/org/formacio/setmana1/mvc/LlibreController.java
@@ -1,6 +1,9 @@
 package org.formacio.setmana1.mvc;
 
+import org.formacio.setmana1.data.LlibreOpsBasic;
+import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,14 +18,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LlibreController {
 
+	@Autowired
+	LlibreOpsBasic llibreopsbasic = new LlibreOpsBasic();
 	// Per aqui vos fara falta una referencia a un LlibreOpsBasic
 	
 	// Aquestes anotacions i firma del metode ja son correctes
 	@RequestMapping(path="/recomanacio")
 	@ResponseBody
  	public Recomanacio obteLlibre (String isbn) {
+		
 		// Feis que retorni la recomanacio per el llibre indicat 
 		// emprat LlibreOpsBasic
-		return null; 
+		return llibreopsbasic.recomenacioPer(isbn); 
 	}
 }
