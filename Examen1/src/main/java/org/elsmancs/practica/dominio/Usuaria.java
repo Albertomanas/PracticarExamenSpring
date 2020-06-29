9ed3b2c468de2d647a94625eac438688c133a9ef
Albertomanas <amanas@cifpfbmoll.eu>
2020-06-29 21:16:39 +0200

    feat(Usuaria): Añadido HashCode y Equals

--- Examen1/src/main/java/org/elsmancs/practica/dominio/Usuaria.java
+++ Examen1/src/main/java/org/elsmancs/practica/dominio/Usuaria.java
@@ -31,4 +31,32 @@ public class Usuaria {
 	public void setDestreza(int destreza) {
 		this.destreza = destreza;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + destreza;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Usuaria))
+			return false;
+		Usuaria other = (Usuaria) obj;
+		if (destreza != other.destreza)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	
+	
 }
