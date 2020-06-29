307f8a542a250a51a914d8eebe7ae9b63ebd5fcb
Albertomanas <amanas@cifpfbmoll.eu>
2020-06-29 21:52:31 +0200

    feat(Repositorio): Detectar Repositorio como repositorio de la api

--- Examen1/src/main/java/org/elsmancs/practica/repositorio/Repositorio.java
+++ Examen1/src/main/java/org/elsmancs/practica/repositorio/Repositorio.java
@@ -1,5 +1,14 @@
 package org.elsmancs.practica.repositorio;
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class Repositorio {
 
+	@PersistenceContext
+	private EntityManager em;
 }
