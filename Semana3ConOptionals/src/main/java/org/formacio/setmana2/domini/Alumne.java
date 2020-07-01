dfbb4229c38b45296ae2996e229526f99ae3081f
Albertomanas <amanas@cifpfbmoll.eu>
2020-07-01 22:29:41 +0200

    feat(Alumne): Mapeo completo

--- Semana3ConOptionals/src/main/java/org/formacio/setmana2/domini/Alumne.java
+++ Semana3ConOptionals/src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -5,4 +5,31 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Entity
+@Table(name = "t_alumnes")
+public class Alumne {
 
+	@Id
+	@Column(name = "alu_nom")
+	private String nombre;
+	
+	@Column(name = "alu_edat")
+	private int edat;
+	
+	public String getNom() {
+		return this.nombre;
+	}
+	
+	public void setNom(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getEdat() {
+		return this.edat;
+	}
+	
+	public void setEdat(int edat) {
+		this.edat = edat;
+	}
+	
+}
