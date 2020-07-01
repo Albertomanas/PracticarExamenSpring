05f0ae106108c82b9becd93bbf48aaed476766a2
Albertomanas <amanas@cifpfbmoll.eu>
2020-07-01 22:24:35 +0200

    feat(dominio): Borradas las clases del mapeo

--- Semana3ConOptionals/src/main/java/org/formacio/setmana2/domini/Alumne.java
+++ Semana3ConOptionals/src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -1,22 +1,8 @@
 package org.formacio.setmana2.domini;
 
-public class Alumne {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-	private String nom;
-	
-	private int edat;
-	
-	public String getNom() {
-		return nom;
-	}
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	public int getEdat() {
-		return edat;
-	}
-	public void setEdat(int edat) {
-		this.edat = edat;
-	}
-	
-}
+
