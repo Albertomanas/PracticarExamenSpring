977fa3a062deb17fe13bb077f064c56c3d9f394d
Albertomanas <amanas@cifpfbmoll.eu>
2020-07-01 23:42:21 +0200

    feat(RepositoriEscola): Método apunta completo con Exception

--- Semana3ConOptionals/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
+++ Semana3ConOptionals/src/main/java/org/formacio/setmana2/repositori/RepositoriEscola.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.formacio.setmana2.domini.Alumne;
 import org.formacio.setmana2.domini.Curs;
@@ -25,16 +26,21 @@ public class RepositoriEscola {
 		return em.find(Curs.class, nom);
 	}
 	
-	
+	@Transactional
 	public Matricula apunta (String alumne, String curs) throws EdatIncorrecteException {
 		Matricula matricula = null;
 		Optional<Alumne> alumnado = Optional.ofNullable(em.find(Alumne.class, alumne));
 		Optional<Curs> curso = Optional.ofNullable(this.carregaCurs(curs));
 		
+		if (alumnado.isPresent() && curso.isPresent()) {
+			if (alumnado.get().getEdat() < curso.get().getEdatMinima())
+				throw new EdatIncorrecteException();
+
 		matricula = new Matricula();
 		matricula.setAlumne(alumnado.get());
 		matricula.setCurs(curso.get());
 		em.persist(matricula);	
+		}
 		return matricula;
 	}
 	
