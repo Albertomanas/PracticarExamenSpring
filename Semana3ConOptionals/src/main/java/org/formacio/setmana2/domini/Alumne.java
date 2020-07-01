b81c20aa771d6a2a219f9b7ed92fc1585ce03f39
Albertomanas <amanas@cifpfbmoll.eu>
2020-07-01 23:07:20 +0200

    feat(Alumne): hashcode y equals a nombre

--- Semana3ConOptionals/src/main/java/org/formacio/setmana2/domini/Alumne.java
+++ Semana3ConOptionals/src/main/java/org/formacio/setmana2/domini/Alumne.java
@@ -31,5 +31,30 @@ public class Alumne {
 	public void setEdat(int edat) {
 		this.edat = edat;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Alumne))
+			return false;
+		Alumne other = (Alumne) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
